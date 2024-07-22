54438e4689248c3756213132f3e9b4860358fdd0
BuildTools <unconfigured@null.spigotmc.org>
2024-07-22 15:45:45 +0800

    v0.0.1

---
+++ js/inject.js
@@ -0,0 +1,26 @@
+const blocklyDiv = document.getElementById('blocklyDiv');
+
+blocklyDiv.style.width = document.body.offsetWidth + 'px';
+blocklyDiv.style.height = document.body.offsetHeight * 0.8 + 'px';
+
+function xml2String(xmlObject) {
+	return (new XMLSerializer()).serializeToString(xmlObject);
+}
+
+$.get("./block/blocks.json",function(data,status){
+    console.log(data);
+    for(i=0;i<data.length;i++){
+        let block = data[i];
+        Blockly.Blocks[block.type] = {
+            init: function() {
+                this.jsonInit(block);
+            }
+        };
+    }
+    $.get("./block/toolbox.xml",function(data,status){
+        const workspace = Blockly.inject(blocklyDiv, {
+            toolbox: xml2String(data),
+            media: './media'
+        });
+    });
+});
