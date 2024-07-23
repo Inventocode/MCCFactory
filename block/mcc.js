40221a12bce570d4953fbbbc480405be3fc58331
Inventocode <359148497@qq.com>
2024-07-23 18:23:55 +0800

    upd

--- block/mcc.js
+++ block/mcc.js
@@ -1,19 +1,19 @@
-var mccGenerator = new Blockly.Generator('mcc');
+Blockly.mcc = new Blockly.Generator('mcc');
 
-mccGenerator['tp'] = function (block) {
-    var value_e = mccGenerator.valueToCode(block, 'e', mccGenerator.ORDER_ATOMIC);
-    var value_p = mccGenerator.valueToCode(block, 'p', mccGenerator.ORDER_ATOMIC);
+Blockly.mcc['tp'] = function (block) {
+    var value_e = Blockly.mcc.valueToCode(block, 'e', Blockly.mcc.ORDER_ATOMIC);
+    var value_p = Blockly.mcc.valueToCode(block, 'p', Blockly.mcc.ORDER_ATOMIC);
     var code = '/tp '+value_e+' '+value_p;
     return code;
 };
 
-mccGenerator['entity'] = function (block) {
+Blockly.mcc['entity'] = function (block) {
     var dropdown_es = block.getFieldValue('es');
     var code = dropdown_es;
     return code;
 };
 
-mccGenerator['pos'] = function (block) {
+Blockly.mcc['pos'] = function (block) {
     var text_x = block.getFieldValue('x');
     var text_y = block.getFieldValue('y');
     var text_z = block.getFieldValue('z');
@@ -21,48 +21,48 @@ mccGenerator['pos'] = function (block) {
     return code;
 };
 
-mccGenerator['player'] = function (block) {
+Blockly.mcc['player'] = function (block) {
     var text_name = block.getFieldValue('name');
     var code = text_name;
     return code;
 };
 
-mccGenerator['path'] = function (block) {
+Blockly.mcc['path'] = function (block) {
     var text_id = block.getFieldValue('id');
     var text_name = block.getFieldValue('name');
     var code = text_id+':'+text_name;
     return code;
 };
 
-mccGenerator['kill'] = function (block) {
-    var value_e = mccGenerator.valueToCode(block, 'e', mccGenerator.ORDER_ATOMIC);
+Blockly.mcc['kill'] = function (block) {
+    var value_e = Blockly.mcc.valueToCode(block, 'e', Blockly.mcc.ORDER_ATOMIC);
     var code = '...;\n';
     return code;
 };
 
-mccGenerator['execute'] = function (block) {
-    var value_sub_command = mccGenerator.valueToCode(block, 'sub_command', mccGenerator.ORDER_ATOMIC);
-    var statements_run_command = mccGenerator.statementToCode(block, 'run_command');
+Blockly.mcc['execute'] = function (block) {
+    var value_sub_command = Blockly.mcc.valueToCode(block, 'sub_command', Blockly.mcc.ORDER_ATOMIC);
+    var statements_run_command = Blockly.mcc.statementToCode(block, 'run_command');
     var code = '...;\n';
     return code;
 };
 
-mccGenerator['time'] = function (block) {
+Blockly.mcc['time'] = function (block) {
     var dropdown_mode = block.getFieldValue('mode');
     var text_time = block.getFieldValue('time');
     var code = '...;\n';
     return code;
 };
 
-mccGenerator['sub_command'] = function (block) {
+Blockly.mcc['sub_command'] = function (block) {
     var text_sub_command = block.getFieldValue('sub_command');
-    var value_more = mccGenerator.valueToCode(block, 'more', mccGenerator.ORDER_ATOMIC);
+    var value_more = Blockly.mcc.valueToCode(block, 'more', Blockly.mcc.ORDER_ATOMIC);
     var code = '...';
     return code;
 };
 
-mccGenerator['selector'] = function (block) {
-    var value_e = mccGenerator.valueToCode(block, 'e', mccGenerator.ORDER_ATOMIC);
+Blockly.mcc['selector'] = function (block) {
+    var value_e = Blockly.mcc.valueToCode(block, 'e', Blockly.mcc.ORDER_ATOMIC);
     var text_selector = block.getFieldValue('selector');
     var code = '...';
     return code;
--- js/inject.js
+++ js/inject.js
@@ -4,23 +4,29 @@ blocklyDiv.style.width = document.body.offsetWidth + 'px';
 blocklyDiv.style.height = document.body.offsetHeight * 0.8 + 'px';
 
 function xml2String(xmlObject) {
-	return (new XMLSerializer()).serializeToString(xmlObject);
+    return (new XMLSerializer()).serializeToString(xmlObject);
 }
 
-$.get("./block/blocks.json",function(data,status){
+$.get("./block/blocks.json", function (data, status) {
     console.log(data);
-    for(i=0;i<data.length;i++){
+    for (i = 0; i < data.length; i++) {
         let block = data[i];
         Blockly.Blocks[block.type] = {
-            init: function() {
+            init: function () {
                 this.jsonInit(block);
             }
         };
     }
-    $.get("./block/toolbox.xml",function(data,status){
-        const workspace = Blockly.inject(blocklyDiv, {
+    $.get("./block/toolbox.xml", function (data, status) {
+        var workspace = Blockly.inject(blocklyDiv, {
             toolbox: xml2String(data),
-            media: './media'
+            media: './media',
+            renderer: 'zelos'
         });
+        workspace.addChangeListener(updateCode);
+        function updateCode(event) {
+            const code = Blockly.mcc.workspaceToCode(workspace);
+            document.getElementById('code').value = code;
+        }
     });
 });
